c1ffdf11d583078c2dbe8c48662544411dd3085c
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-05 19:34:48 +0200

    Initial

---
+++ Back-end/HSForumAPI/HSForumAPI/Program.cs
@@ -0,0 +1,88 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace HSForumAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("_allowAll",
+                    policy =>
+                    {
+                        policy
+                          .AllowAnyHeader()
+                          .AllowAnyOrigin()
+                          .AllowAnyMethod();
+                    });
+            });
+
+            builder.Services.AddSwaggerGen(option =>
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                option.IncludeXmlComments(xmlPath);
+
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Description =
+                        "JWT Authorization header is using Bearer scheme. \r\n\r\n" +
+                        "Enter 'Bearer' and token separated by a space. \r\n\r\n" +
+                        "Example: \"Bearer d5f41g85d1f52a\"",
+                    Name = "Authorization", 
+                    In = ParameterLocation.Header,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT"
+                });
+
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
