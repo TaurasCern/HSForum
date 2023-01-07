e9509f167577fbcd37d0586bb65bdacdad26f59b
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-07 15:37:39 +0200

    added JwtService and PasswordService interfaces

---
+++ Back-end/HSForumAPI/HSForumAPI.Domain/Services/IServices/IJwtService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSForumAPI.Domain.Services.IServices
+{
+    public interface IJwtService
+    {
+        string GetJwtToken(int userId, string roles[]);
+    }
+}
