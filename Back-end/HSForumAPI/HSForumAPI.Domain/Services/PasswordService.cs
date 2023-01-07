0f78277d3dce14edf69b4bfdb1b22968873f30cc
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-08 00:32:30 +0200

    Implemented PasswordService

---
+++ Back-end/HSForumAPI/HSForumAPI.Domain/Services/PasswordService.cs
@@ -0,0 +1,27 @@
+﻿using HSForumAPI.Domain.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSForumAPI.Domain.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA256();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+        public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA256(passwordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            return computedHash.SequenceEqual(passwordHash);
+        }
+    }
+}
