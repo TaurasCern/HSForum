bb6ef979de12f5b2f9798b734aed4e929e874493
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-09 13:47:02 +0200

    Split if statement

--- Back-end/HSForumAPI/HSForumAPI.Infrastructure/Repositories/UserRepository.cs
+++ Back-end/HSForumAPI/HSForumAPI.Infrastructure/Repositories/UserRepository.cs
@@ -48,7 +48,12 @@ namespace HSForumAPI.Infrastructure.Repositories
                 .ThenInclude(ur => ur.Role)
                 .FirstOrDefaultAsync(u => u.Username.ToLower() == loginRequest.Username.ToLower());
 
-            if (user == null && !_passwordService.VerifyPasswordHash(loginRequest.Password, user.PasswordHash, user.PasswordSalt))
+            if (user == null)
+            {
+                return null;
+            }
+
+            if(!_passwordService.VerifyPasswordHash(loginRequest.Password, user.PasswordHash, user.PasswordSalt))
             {
                 return null;
             }
