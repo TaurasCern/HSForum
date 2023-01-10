08db1fa4072af6748a5b893e5cb534eef669ee59
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-10 13:28:29 +0200

    Added ModelState validation for existing Dtos

--- Back-end/HSForumAPI/HSForumAPI.Domain/DTOs/PostDTOs/PostRequest.cs
+++ Back-end/HSForumAPI/HSForumAPI.Domain/DTOs/PostDTOs/PostRequest.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HSForumAPI.Domain.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,25 @@ namespace HSForumAPI.Domain.DTOs.PostDTOs
 {
     public class PostRequest
     {
+        /// <summary>
+        /// Title of the post
+        /// </summary>
+        [Required]
+        [Range(3,50, ErrorMessage = "Title length has to be from 3 to 50 characters long")]
         public string Title { get; set; }
+        /// <summary>
+        /// Contents of the post
+        /// </summary>
+        [Required]
+        [MaxLength(1000, ErrorMessage = "Content cannot be longer 1000 characters")]
         public string Content { get; set; }
+        /// <summary>
+        /// Enum value of the post
+        /// 1. News
+        /// </summary>
+        [Required]
+        [Range(3, 50, ErrorMessage = "Title length has to be from 3 to 50 characters long")]
+        [EnumDataType(typeof(EPostType))]
         public string PostType { get; set; }
     }
 }
