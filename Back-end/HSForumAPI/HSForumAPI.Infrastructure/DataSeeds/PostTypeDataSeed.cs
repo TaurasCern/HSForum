5527f0cca5cfdd9d9ee4f80ea2c4a096cb748ef7
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-10 14:43:29 +0200

    Added PostType dataseed

---
+++ Back-end/HSForumAPI/HSForumAPI.Infrastructure/DataSeeds/PostTypeDataSeed.cs
@@ -0,0 +1,21 @@
+﻿using HSForumAPI.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSForumAPI.Infrastructure.DataSeeds
+{
+    public class PostTypeDataSeed
+    {
+        public readonly static PostType[] Data = new PostType[1]
+        {
+            new PostType()
+            {
+                PostTypeId = 1,
+                Type = Domain.Enums.EPostType.News
+            },
+        };
+    }
+}
