b28e2585a717e8e25e2d31a205f8211e3446fa6f
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-10 18:54:47 +0200

    Added PosetGetResponse, PostGetTypeRequest, PostReplyResponse DTOs

---
+++ Back-end/HSForumAPI/HSForumAPI.Domain/DTOs/PostReplyDTOs/PostReplyResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace HSForumAPI.Domain.DTOs.PostReplyDTOs
+{
+    public class PostReplyResponse
+    {
+        public int ReplyId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int UserId { get; set; }
+    }
+}
