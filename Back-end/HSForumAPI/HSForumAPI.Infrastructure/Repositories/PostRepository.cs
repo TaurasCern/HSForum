5ce2c2949de049feb259a5d7fcc305fe49be3563
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-10 20:49:15 +0200

    Implemented method to get Post with Replies included

--- Back-end/HSForumAPI/HSForumAPI.Infrastructure/Repositories/PostRepository.cs
+++ Back-end/HSForumAPI/HSForumAPI.Infrastructure/Repositories/PostRepository.cs
@@ -1,9 +1,11 @@
 ﻿using HSForumAPI.Domain.Models;
 using HSForumAPI.Infrastructure.Database;
 using HSForumAPI.Infrastructure.Repositories.IRepositories;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +18,23 @@ namespace HSForumAPI.Infrastructure.Repositories
         public PostRepository(HSForumContext db) : base(db)
         {
             _db = db;
+        }
+        public async Task<Post> GetWithReplies(Expression<Func<Post, bool>> filter, bool tracked = true)
+        {
+            IQueryable<Post> query = _db.Posts
+                .Include(p => p.Replies);
+
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return await query.FirstOrDefaultAsync();
         }
     }
 }
