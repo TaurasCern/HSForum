11ba69ae74a452d4f2dc41d47273cde60245c009
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-10 21:38:09 +0200

    Created Rating DTOs

---
+++ Back-end/HSForumAPI/HSForumAPI.Domain/DTOs/RatingDTOs/RatingResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSForumAPI.Domain.DTOs.RatingDTOs
+{
+    public class RatingResponse
+    {
+        /// <summary>
+        /// Status of the rating that was given
+        /// </summary>
+        public bool IsPositive { get; set; }
+        /// <summary>
+        /// Post id of the post that was rated
+        /// </summary>
+        public int PostId { get; set; }
+
+    }
+}
