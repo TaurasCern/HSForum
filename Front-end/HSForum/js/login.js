6b2593b8727c74c2b7fec849901654483f8050e8
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-11 02:30:09 +0200

    Initial + Cors fix

---
+++ Front-end/HSForum/js/login.js
@@ -0,0 +1,48 @@
+window.onload = () => {
+    insertHeader();
+    if(!(localStorage.getItem(`token`) === null)){
+        window.location.assign("index.html");
+    }
+
+    document.querySelector(`#login-button`)
+        .addEventListener(`click`, (e) => {
+            e.preventDefault();
+            login();
+    })
+}
+
+const login = () => {
+    let loginForm = document.querySelector(`.login-form`);
+    let data = getFormData(loginForm);
+    
+    loginFetch(data.username, data.password);
+}
+const getFormData = (form) => {
+    let formData = new FormData(form);
+    let data = {};
+
+    formData.forEach((value, key) => data[key] = value);
+    
+    return data;
+}
+
+const loginFetch = async (username, password) => {
+    let response = await fetch(`http://localhost:5084/api/Login`, {
+        method: `post`,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password,
+        })
+    })
+
+    if(response.ok){
+        let json = await response.json();
+        localStorage.setItem(`token`, json.token);
+        document.location.assign(`index.html`);
+    }
+    else console.log(response.status);
+}
