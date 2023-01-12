b83c7ea84908731ae7d42590a3a754d0acd148b2
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-12 20:46:55 +0200

    Refactoring

--- Front-end/HSForum/js/posts.js
+++ Front-end/HSForum/js/posts.js
@@ -1,20 +1,23 @@
+let type;
 window.onload = () => {
     insertHeader();
  
     let params = new Proxy(new URLSearchParams(window.location.search), {
         get: (searchParams, prop) => searchParams.get(prop),
     });
+    type = params.type;
 
-    if(params.type != null){
-        loadPosts(params.type);
+    if(type != null){
+        loadPosts(type);
+        loadEventListeners();
     }
     else window.location.assign("index.html");
 }
 
-const loadPosts = (type) => {
-    fetchPosts(type);
+const loadEventListeners = () => {
+    loadCreatePostButton();
 }
-const fetchPosts = async (type) => {
+const loadPosts = async (type) => {
     let response = await fetch(`http://localhost:5084/api/Post/${type}`, {
         method: `get`,
         headers: {
@@ -45,3 +48,64 @@ const insertPost = (container, post) => {
     </div>
     `;
 }
+
+const loadCreatePostButton = () => {
+    document.querySelector(`.post-open-button`).addEventListener(`click`, () => {
+        if(document.querySelector(`.create-post-form`) === null){
+            insertCreatePost();
+        }
+        else{
+            document.querySelector(`.create-post-form`).remove();
+        }
+    });
+};
+
+const insertCreatePost = () => {
+    document.querySelector(`.post-open-button`).outerHTML +=`
+        <form class="create-post-form">
+            <label for="title">Title:</label>
+            <input type="text" name="title" id="title" autocomplete="off">
+
+            <label for="content">Content:</label>
+            <textarea name="content" id="content cols="10" rows="10"></textarea>
+
+            <input type="button" name="post-button" id="post-button" value="Post">
+        </form>
+    `;
+    loadPostButtonEventListener();
+    loadEventListeners();
+};
+
+const loadPostButtonEventListener = () => {
+    document.querySelector(`#post-button`).addEventListener(`click`, (e) => {
+        e.preventDefault();
+        post();
+    })
+};
+
+const post = async () => {
+    let formData = new FormData(document.querySelector(`.create-post-form`));
+    let data = {};
+
+    formData.forEach((value, key) => data[key] = value);
+
+    let response = await fetch(`http://localhost:5084/api/Post`, {
+        method: `post`,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem(`token`)}`
+        },
+        body: JSON.stringify({
+            title: data.title,
+            content: data.content,
+            postType: type
+        })
+    })
+
+    if(response.ok){
+
+        window.location.reload()
+    }
+    else console.log(response.status);
+};
