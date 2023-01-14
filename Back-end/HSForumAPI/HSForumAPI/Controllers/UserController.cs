7299d750468178c0b499f024b27963b185a7dea4
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-14 16:30:28 +0200

    Revert "Refactored Login and Register endpoints to be more testable"
    This reverts commit fd8c793d7a49b71a1b985ea62cb477b282869662.

--- Back-end/HSForumAPI/HSForumAPI/Controllers/UserController.cs
+++ Back-end/HSForumAPI/HSForumAPI/Controllers/UserController.cs
@@ -1,6 +1,4 @@
 ﻿using HSForumAPI.Domain.DTOs.UserDTOs;
-using HSForumAPI.Domain.Models;
-using HSForumAPI.Domain.Services.IServices;
 using HSForumAPI.Infrastructure.Repositories.IRepositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,55 +10,34 @@ namespace HSForumAPI.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUnitOfWork _db;
-        private readonly IJwtService _jwtService;
-        private readonly IPasswordService _passwordService;
-        public UserController(IUnitOfWork db, IJwtService jwtService, IPasswordService passwordService)
+        public UserController(IUnitOfWork db)
         {
             _db = db;
-            _jwtService = jwtService;
-            _passwordService = passwordService;
         }
         [HttpPost("/api/Login")]
-        [ProducesResponseType(200, Type = typeof(LoginResponse))]
+        [ProducesResponseType(200)]
         [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
         [ProducesResponseType(500)]
-        public async Task<IActionResult> Login([FromBody] LoginRequest req)
+        public async Task<ActionResult<LoginResponse>> Login([FromBody] LoginRequest req)
         {
-            var result = await _db.Users.TryLoginAsync(req.Username, req.Password);
+            var token = await _db.Users.LoginAsync(req);
 
-            if(!result.Item1) 
-                return Unauthorized();
+            if(token == null) return BadRequest();
 
-            var token = _jwtService
-                .GetJwtToken(result.Item2.UserId, 
-                    result.Item2.UserRoles
-                        .Select(ur => ur.Role.Name.ToString()).ToArray());
-            return Ok(new { token });
+            return Ok(new { token = token });
         }
         [HttpPost("/api/Register")]
-        [ProducesResponseType(200, Type = typeof(LoginResponse))]
-        [ProducesResponseType(201)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(200)]
         [ProducesResponseType(500)]
-        public async Task<IActionResult> Register([FromBody] RegistrationRequest req)
+        public async Task<ActionResult<LoginResponse>> Register([FromBody] RegistrationRequest req)
         {
             if (await _db.Users.IsRegisteredAsync(req.Username, req.Email)) 
-                return BadRequest("User already exists");
+                return BadRequest(new {message = "Username or email already exists" });
 
-            _passwordService.CreatePasswordHash(req.Password, out var passwordHash, out var passwordSalt);
+            var token = await _db.Users.RegisterAsync(req);
 
-            var user = new LocalUser
-            {
-                Username = req.Username,
-                Email = req.Email,
-                CreatedAt = DateTime.Now,
-                PasswordHash = passwordHash,
-                PasswordSalt = passwordSalt
-            };
-
-            var id = await _db.Users.RegisterAsync(user);
-
-            return Created(nameof(Login), new { id });
+            return Ok(new { token = token });
         }
     }
 }
