4b72a38f3f2599294b6927139ee9ad0f5a1ab13f
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-14 16:22:30 +0200

    Updated context for HasData and updated Post model

--- Back-end/HSForumAPI/HSForumAPI.Infrastructure/Migrations/20230114134011_PostAddedUpdatedAt.Designer.cs
+++ Back-end/HSForumAPI/HSForumAPI.Infrastructure/Migrations/20230114134011_PostAddedUpdatedAt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HSForumAPI.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HSForumAPI.Infrastructure.Migrations
 {
     [DbContext(typeof(HSForumContext))]
-    partial class HSForumContextModelSnapshot : ModelSnapshot
+    [Migration("20230114134011_PostAddedUpdatedAt")]
+    partial class PostAddedUpdatedAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.12");
---
+++ Back-end/HSForumAPI/HSForumAPI.Infrastructure/Migrations/20230114134011_PostAddedUpdatedAt.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HSForumAPI.Infrastructure.Migrations
+{
+    public partial class PostAddedUpdatedAt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRole_Role_RoleId",
+                table: "UserRole");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRole_Users_UserId",
+                table: "UserRole");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRole",
+                table: "UserRole");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Role",
+                table: "Role");
+
+            migrationBuilder.RenameTable(
+                name: "UserRole",
+                newName: "UserRoles");
+
+            migrationBuilder.RenameTable(
+                name: "Role",
+                newName: "Roles");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRole_UserId",
+                table: "UserRoles",
+                newName: "IX_UserRoles_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRole_RoleId",
+                table: "UserRoles",
+                newName: "IX_UserRoles_RoleId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Posts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles",
+                column: "UserRoleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Roles",
+                table: "Roles",
+                column: "RoleId");
+
+            migrationBuilder.InsertData(
+                table: "PostTypes",
+                columns: new[] { "PostTypeId", "Type" },
+                values: new object[] { 1, "News" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "Name" },
+                values: new object[] { 1, "User" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "Name" },
+                values: new object[] { 2, "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "Name" },
+                values: new object[] { 3, "Moderator" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "CreatedAt", "Email", "PasswordHash", "PasswordSalt", "Username" },
+                values: new object[] { 1, new DateTime(2023, 1, 14, 15, 40, 11, 342, DateTimeKind.Local).AddTicks(6240), "admin@hsforum.lt", new byte[] { 18, 34, 181, 66, 55, 217, 63, 31, 36, 99, 199, 97, 124, 53, 22, 244, 70, 61, 99, 43, 97, 105, 220, 178, 65, 54, 131, 242, 176, 128, 1, 164 }, new byte[] { 59, 178, 26, 240, 147, 28, 10, 42, 13, 31, 82, 129, 11, 249, 114, 142, 206, 68, 65, 227, 111, 201, 205, 70, 97, 242, 17, 154, 193, 182, 171, 29, 85, 231, 18, 130, 148, 103, 105, 11, 52, 195, 131, 12, 251, 230, 117, 131, 95, 135, 191, 187, 167, 69, 222, 1, 30, 252, 55, 182, 183, 97, 21, 79 }, "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "UserRoles",
+                columns: new[] { "UserRoleId", "RoleId", "UserId" },
+                values: new object[] { 1, 2, 1 });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "RoleId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                table: "UserRoles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                table: "UserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Roles",
+                table: "Roles");
+
+            migrationBuilder.DeleteData(
+                table: "PostTypes",
+                keyColumn: "PostTypeId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "UserRoles",
+                keyColumn: "UserRoleId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1);
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Posts");
+
+            migrationBuilder.RenameTable(
+                name: "UserRoles",
+                newName: "UserRole");
+
+            migrationBuilder.RenameTable(
+                name: "Roles",
+                newName: "Role");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRoles_UserId",
+                table: "UserRole",
+                newName: "IX_UserRole_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRoles_RoleId",
+                table: "UserRole",
+                newName: "IX_UserRole_RoleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRole",
+                table: "UserRole",
+                column: "UserRoleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Role",
+                table: "Role",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRole_Role_RoleId",
+                table: "UserRole",
+                column: "RoleId",
+                principalTable: "Role",
+                principalColumn: "RoleId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRole_Users_UserId",
+                table: "UserRole",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
