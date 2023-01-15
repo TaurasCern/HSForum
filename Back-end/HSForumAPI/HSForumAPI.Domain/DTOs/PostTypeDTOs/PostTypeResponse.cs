9328223c4673cdb44b03cdcdf58fbf29adf0e850
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-15 02:39:45 +0200

    Created PostTypeResponse dto

---
+++ Back-end/HSForumAPI/HSForumAPI.Domain/DTOs/PostTypeDTOs/PostTypeResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSForumAPI.Domain.DTOs.PostTypeDTOs
+{
+    public class PostTypeResponse
+    {
+        /// <summary>
+        /// Type of the post
+        /// </summary>
+        public string Type { get; set; }
+    }
+}
