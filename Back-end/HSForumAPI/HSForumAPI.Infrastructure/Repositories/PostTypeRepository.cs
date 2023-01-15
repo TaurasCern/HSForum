f297bf2e8e64c5894d2be114d1cc60457730d530
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-15 02:36:24 +0200

    Added PostType Repository

---
+++ Back-end/HSForumAPI/HSForumAPI.Infrastructure/Repositories/PostTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using HSForumAPI.Domain.Models;
+using HSForumAPI.Infrastructure.Database;
+using HSForumAPI.Infrastructure.Repositories.IRepositories;
+
+namespace HSForumAPI.Infrastructure.Repositories
+{
+    public class PostTypeRepository : Repository<PostType>, IPostTypeRepository
+    {
+        private readonly HSForumContext _db;
+        public PostTypeRepository(HSForumContext db)
+            : base(db)
+        {
+            _db = db;
+        }
+    }
+}
