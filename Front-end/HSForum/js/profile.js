86d249722d6bb04d913addf778fbc113717b5d3d
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-15 02:44:28 +0200

    Added account display

--- Front-end/HSForum/js/profile.js
+++ Front-end/HSForum/js/profile.js
@@ -1,8 +1,28 @@
 window.onload = () => {
     insertHeader();
 
+    loadUser();
+
     document.querySelector(`#logout-button`).addEventListener(`click`, (e) =>{
         localStorage.removeItem(`token`);
+        localStorage.removeItem(`roles`);
+        localStorage.removeItem(`id`);
         window.location.assign(`index.html`);
     })
+}
+
+const loadUser = async () => {
+    let response = await fetch(`http://localhost:5084/api/User/${localStorage.getItem(`id`)}`, {
+        method: `get`,
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem(`token`)}`
+        },
+    })
+
+    if(response.ok){
+        let json = await response.json();
+        document.querySelector(`.account-container`).innerHTML = `
+        ${json.username}${json.createdAt}${json.reputation}    
+        `;
+    }
 }
