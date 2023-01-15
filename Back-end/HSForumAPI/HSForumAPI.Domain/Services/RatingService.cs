f93edd12a3b463e7acd1ea045853c37270404c52
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-01-15 23:20:26 +0200

    Added comments to RatingService

--- Back-end/HSForumAPI/HSForumAPI.Domain/Services/RatingService.cs
+++ Back-end/HSForumAPI/HSForumAPI.Domain/Services/RatingService.cs
@@ -5,11 +5,20 @@ namespace HSForumAPI.Domain.Services
 {
     public class RatingService : IRatingService
     {
+        /// <summary>
+        /// Calculates rating using Post=>Rating
+        /// </summary>
+        /// <param name="post"></param>
+        /// <returns>Rating count</returns>
         public async Task<int> CalculateRating(Post post)
             => post.Ratings == null ? 0 
                 : post.Ratings.Select(r => r.IsPositive ? 1 : -1)
                     .Sum();
-
+        /// <summary>
+        /// Calculates total rating out of given posts
+        /// </summary>
+        /// <param name="posts"></param>
+        /// <returns></returns>
         public async Task<int> CalculateUserReputation(List<Post> posts)
         {
             var reputation = 0;
@@ -19,7 +28,12 @@ namespace HSForumAPI.Domain.Services
             }
             return reputation;
         }
-
+        /// <summary>
+        /// Check if post was rated by the given user id
+        /// </summary>
+        /// <param name="post"></param>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         public async Task<bool> CheckIfRated(Post post, int userId) 
             => post.Ratings != null 
             && post.Ratings
